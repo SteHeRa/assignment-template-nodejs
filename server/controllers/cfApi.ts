d7cb3d01f5ec3d2d3096585fc2acd5ee17e6ab30
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-11-22 18:32:40 +0000

    fix: minor type checking in controller

--- server/controllers/cfApi.ts
+++ server/controllers/cfApi.ts
@@ -3,45 +3,55 @@ import { Request, Response } from 'express';
 
 const cfControl = {
   getNotices: async (req: Request, res: Response) => {
-    const { from, to } = req.query; //range of dates we want to find notices for
+    console.log('called');
     try {
-      const notices = await fetch(
-        'https://www.contractsfinder.service.gov.uk/api/rest/2/search_notices/json',
-        {
-          method: 'post',
-          body: JSON.stringify({
-            searchCriteria: {
-              types: ['Contract'],
-              statuses: ['Open'],
-              keyword: null,
-              queryString: null,
-              regions: null,
-              postcode: null,
-              radius: null,
-              valueFrom: null,
-              valueTo: null,
-              lastNotifiableUpdateFrom: null,
-              publishedFrom: from,
-              publishedTo: to,
-              deadlineFrom: null,
-              deadlineTo: null,
-              approachMarketFrom: null,
-              approachMarketTo: null,
-              awardedFrom: null,
-              awardedTo: null,
-              isSubcontract: null,
-              suitableForSme: null,
-              suitableForVco: null,
-              awardedToSme: null,
-              awardedToVcse: null,
-              cpvCodes: null,
-            },
-            size: 1000,
-          }),
-          headers: { 'Content-Type': 'application/json' },
-        }
-      ).then((res) => res.json());
-      res.send(notices.noticeList);
+      if (
+        typeof req.query.from === 'string' &&
+        typeof req.query.to === 'string'
+      ) {
+        const from = new Date(req.query.from);
+        const to = new Date(req.query.to);
+        const notices = await fetch(
+          'https://www.contractsfinder.service.gov.uk/api/rest/2/search_notices/json',
+          {
+            method: 'post',
+            body: JSON.stringify({
+              searchCriteria: {
+                types: ['Contract'],
+                statuses: ['Open'],
+                keyword: null,
+                queryString: null,
+                regions: null,
+                postcode: null,
+                radius: null,
+                valueFrom: null,
+                valueTo: null,
+                lastNotifiableUpdateFrom: null,
+                publishedFrom: from,
+                publishedTo: to,
+                deadlineFrom: null,
+                deadlineTo: null,
+                approachMarketFrom: null,
+                approachMarketTo: null,
+                awardedFrom: null,
+                awardedTo: null,
+                isSubcontract: null,
+                suitableForSme: null,
+                suitableForVco: null,
+                awardedToSme: null,
+                awardedToVcse: null,
+                cpvCodes: null,
+              },
+              size: 1000,
+            }),
+            headers: { 'Content-Type': 'application/json' },
+          }
+        ).then((res) => res.json());
+        res.status(200);
+        res.send(notices.noticeList);
+      } else {
+        res.send([]);
+      }
     } catch (err) {
       console.log('---> error getting notices from API', err);
       res.status(500);
