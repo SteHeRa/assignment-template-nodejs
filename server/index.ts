39472c33345a6e56ad7589a26de87867eb15282e
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-11-22 18:31:39 +0000

    feat: added cors

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './router';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = process.env.PORT || 1234;
 
+app.use(cors());
 app.use('/', router);
 
 app.listen(PORT, () => {
